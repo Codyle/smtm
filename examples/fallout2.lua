49ad2442c15541a2d270f1e039d47efde4cbeeca
HE Yaowen <he.yaowen@hotmail.com>
2016-07-25 19:21:13 +0800

    Add example script to get character information from game Fallout 2

---
+++ examples/fallout2.lua
@@ -0,0 +1,23 @@
+-- Finds the Fallout window
+local wnd = wapi.FindWindow(nil, "FALLOUT II")
+
+-- Finds the process ID
+local result, pid = wapi.GetWindowThreadProcessId(wnd)
+
+-- Opens the process
+local proc = wapi.OpenProcess(nil, 0, pid)
+
+-- Reads current experience
+local status, result, bytes = wapi.ReadProcessMemory(proc, 0x00678724, 4)
+
+print("Reading current experience...")
+print("Status: " .. tostring(status))
+print("Experience: " .. macaco.udatatointeger(result))
+print("Bytes read: " .. bytes)
+print()
+
+if status then
+    print("Writing current experience...")
+    status = wapi.WriteProcessMemory(proc, 0x00678724, macaco.integertoudata(100), 4)
+    print("Status: " .. tostring(status))
+end
